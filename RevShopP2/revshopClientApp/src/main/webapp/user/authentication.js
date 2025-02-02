d2741611af5ddd48812ec090df8a04f3cf66ef58
SivaGanesh <99261588+SIVAGANESH191988@users.noreply.github.com>
2025-02-02 11:44:56 +0530

    Update authentication.js

--- RevShopP2/revshopClientApp/src/main/webapp/user/authentication.js
+++ RevShopP2/revshopClientApp/src/main/webapp/user/authentication.js
@@ -14,10 +14,14 @@ setTimeout(() => {
 
 
 window.onload = function() {
-            var message = "${message}"; // Get the message from request attributes
+            var message = "${message}"; 
+// Get the message from request attributes
             if (message) {
-                document.getElementById("alertMessage").innerText = message; // Set the alert message
-                document.getElementById("alertContainer").style.display = "block"; // Show the alert
-                $('#alertContainer').fadeIn(); // Fade in the alert for smoothness
+                document.getElementById("alertMessage").innerText = message; 
+// Set the alert message
+                document.getElementById("alertContainer").style.display = "block";
+ // Show the alert
+                $('#alertContainer').fadeIn();
+ // Fade in the alert for smoothness
             }
         };
